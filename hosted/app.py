5f4c7aee4ca49422d3316cd27c7d2cff274cc9c1
lillian <lillianxxzhang@gmail.com>
2024-10-30 13:19:52 +1100

    update filepath

--- hosted/app.py
+++ hosted/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__) # invoke the Flask class
 def get_iris():
     pred_names = ['Iris Setosa', 'Iris Versicolor', 'Iris Virginica']
     pred_label = ''
-    filename = '/home/lillianzhang/iris-dnn/hosted/dnn_model.pkl'
+    filename = 'dnn_model.pkl'
     with open(filename, 'rb') as file:
     
         # load model
